bec0af87c34b5bad1ba3ef0691c28c896cc8b946
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-10 18:27:24 +0200

    first commit

---
+++ src/main/java/com/githubuserbranchesapi/GitHubUserBranchesApiApplication.java
@@ -0,0 +1,13 @@
+package com.githubuserbranchesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubUserBranchesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubUserBranchesApiApplication.class, args);
+    }
+
+}
