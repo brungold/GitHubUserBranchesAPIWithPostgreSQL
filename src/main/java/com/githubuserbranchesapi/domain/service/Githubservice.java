ab51433abc34eca69f949f261250c76e3aa52fad
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-03 14:32:25 +0200

    added to the Githubservice method  getAllRepositoryNames thet took repo names list from Api Github -> saved it to db and gave it back as list RequiredResponseDto

--- src/main/java/com/githubuserbranchesapi/domain/service/Githubservice.java
+++ src/main/java/com/githubuserbranchesapi/domain/service/Githubservice.java
@@ -1,9 +1,43 @@
 package com.githubuserbranchesapi.domain.service;
 
+import com.githubuserbranchesapi.client.GithubProxy;
+import com.githubuserbranchesapi.domain.dto.GithubRepoResponseDto;
+import com.githubuserbranchesapi.domain.dto.RequiredResponseDto;
+import com.githubuserbranchesapi.domain.model.Repo;
+import com.githubuserbranchesapi.repository.GithubRepoRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
 public class Githubservice {
+    private final GithubProxy githubClient;
+    private final GithubRepoRepository githubRepoRepository;
+
+    public Githubservice(GithubProxy githubClient, GithubRepoRepository githubRepoRepository) {
+        this.githubClient = githubClient;
+        this.githubRepoRepository = githubRepoRepository;
+    }
+
+    public List<RequiredResponseDto> getAllRepositoryNames(String username) {
+        //Pobieram listę repozytoriów użytkownika z API GitHub
+        List<GithubRepoResponseDto> allUserRepositories = githubClient.getUserRepos(username);
+
+        //Przetwarzamdane i zapisz je do bazy danych oraz zwróć listę RequiredResponseDto
+        List<RequiredResponseDto> requiredResponseList = allUserRepositories.stream()
+                .map(repoDto -> {
+                    //Zapisuje  informacje o repozytorium do bazy danych
+                    Repo repo = new Repo(repoDto.owner().login(), repoDto.name());
+                    Repo savedRepo = githubRepoRepository.save(repo);
+
+                    //Tworze RequiredResponseDto na podstawie danych zapisanych w bazie danych
+                    return new RequiredResponseDto(savedRepo.getId(), savedRepo.getOwner(), savedRepo.getName());
+                })
+                .collect(Collectors.toList());
+
+        return requiredResponseList;
+    }
 }
